94260e062b899eaf757f96285f42757e609429f8
one4ALLover <egorrvanijked@gmail.com>
2024-11-25 12:01:31 +0300

    1st_n_2nd_t_with_extra_done

--- main.cpp
+++ main.cpp
@@ -1,7 +1,93 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include "stdio.h"
+#include "stdlib.h"
 #include "cstdlib"
+
+struct node {
+	int vertex;
+	struct node* next;
+};
+
+struct Graph {
+	int numVertices;
+	struct node** adjLists;
+};
+
+struct node* createNode(int v) {
+	struct node* newNode = (node*)malloc(sizeof(struct node));
+	newNode->vertex = v;
+	newNode->next = NULL;
+	return newNode;
+}
+
+struct Graph* createAGraph(int vertices) {
+	struct Graph* graph = (Graph*)malloc(sizeof(struct Graph));
+	graph->numVertices = vertices;
+
+	graph->adjLists = (node**)malloc(vertices * sizeof(struct node*));
+
+	for (int i = 0; i < vertices; i++) {
+		graph->adjLists[i] = NULL;
+	}
+
+	return graph;
+}
+
+void addEdge(struct Graph* graph, int s, int d) {
+	struct node* temp = graph->adjLists[s];
+	while (temp) {
+		if (temp->vertex == d) {
+			return;
+		}
+		temp = temp->next;
+	}
+
+	struct node* newNode = createNode(d);
+	newNode->next = graph->adjLists[s];
+	graph->adjLists[s] = newNode;
+
+	temp = graph->adjLists[d];
+	while (temp) {
+		if (temp->vertex == s) {
+			return;
+		}
+		temp = temp->next;
+	}
+
+	newNode = createNode(s);
+	newNode->next = graph->adjLists[d];
+	graph->adjLists[d] = newNode;
+}
+
+
+struct Graph* edges(int** G, int size) {
+	struct Graph* graph = createAGraph(size);
+
+	for (int i = 0; i < size; i++) {
+		for (int j = i; j < size; j++) {
+			if (G[i][j] == 1) {
+				addEdge(graph, i, j);
+			}
+		}
+	}
+	return graph;
+}
+
+void printGraph(struct Graph* graph) {
+	for (int v = 0; v < graph->numVertices; v++) {
+		struct node* temp = graph->adjLists[v];
+		printf("\nVertex %d: ", v + 1);
+		while (temp) {
+			printf("%d -> ", temp->vertex + 1);
+			temp = temp->next;
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+
+
 
 int** createG(int size) {
 	int** G = NULL;
@@ -30,142 +116,239 @@ void printG(int** G, int size) {
 	return;
 }
 
-int** delV(int** G, int size, int v) {
-	int** Gtemp = createG(size - 1);
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size; j++) {
-			if (i < v && j < v) Gtemp[i][j] = G[i][j];
-			if (i > v && j > v) Gtemp[i - 1][j - 1] = G[i][j];
-			if (i > v && j < v) Gtemp[i - 1][j] = G[i][j];
-			if (i < v && j > v) Gtemp[i][j - 1] = G[i][j];
+
+
+void unionV(struct Graph* graph, int v1, int v2) {
+	if (v1 == v2) return;
+
+	struct node* temp = graph->adjLists[v2];
+	while (temp) {
+		addEdge(graph, v1, temp->vertex);
+		temp = temp->next;
+	}
+
+	for (int i = 0; i < graph->numVertices; i++) {
+		struct node* adj = graph->adjLists[i];
+		struct node* p = NULL;
+
+		while (adj) {
+			if (adj->vertex == v2) {
+				if (p) {
+					p->next = adj->next;
+				}
+				else {
+					graph->adjLists[i] = adj->next;
+				}
+				struct node* del = adj;
+				adj = adj->next;
+				free(del);
+			}
+			else {
+				p = adj;
+				adj = adj->next;
+			}
 		}
 	}
-	for (int i = 0; i < size; i++) {
-		free(G[i]);
+
+	graph->adjLists[v2] = NULL;
+
+	for (int i = v2 + 1; i < graph->numVertices; i++) {
+		graph->adjLists[i - 1] = graph->adjLists[i];
 	}
-	free(G);
-	G = NULL;
+	graph->adjLists[graph->numVertices - 1] = NULL;
 
-	return Gtemp;
-}
+	graph->numVertices--;
 
-int** unionV(int** G, int size, int v1, int v2) {
-	for (int i = 0; i < size; i++) {
-		if (G[v2][i] == 1) {
-			G[v1][i] = G[v2][i];
-			G[i][v1] = G[i][v2];
+	for (int i = 0; i < graph->numVertices; i++) {
+		struct node* adj = graph->adjLists[i];
+		while (adj) {
+			if (adj->vertex > v2) {
+				adj->vertex--;
+			}
+			adj = adj->next;
 		}
 	}
-
-	G = delV(G, size, v2);
-	return G;
 }
 
-int** contrE(int** G, int size, int v1, int v2) {
-	if (G[v1][v2] != 1) {
-		printf("reb no");
-		return G;
-	}
-	else {
-		G[v1][v2] = 0;
-		G[v2][v1] = 0;
+void styag(struct Graph* graph, int v1, int v2) {
+	if (v1 == v2) return;
 
-		for (int i = 0; i < size; i++) {
-			if (G[v2][i] == 1) {
-				G[v1][i] = G[v2][i];
-				G[i][v1] = G[i][v2];
-
-			}////////
+	int flag = 0;
+	struct node* temp = graph->adjLists[v1];
+	while (temp) {
+		if (temp->vertex == v2) {
+			flag = 1;
 		}
-		G = delV(G, size, v2);
-
-		return G;
-
+		temp = temp->next;
 	}
-}
 
-int** splitV(int** G, int size, int v) {
-	int** Gtemp = createG(size + 1);
-	for (int i = 0; i < size; i++) {
-		Gtemp[i][size] = G[i][v];
-		Gtemp[size][i] = G[v][i];
-	}
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size; j++) {
-			Gtemp[i][j] = G[i][j];
+	if (flag) {
+		struct node* adj = graph->adjLists[v1];
+		struct node* p = NULL;
+
+		while (adj) {
+			if (adj->vertex == v2) {
+				if (p) {
+					p->next = adj->next;
+				}
+				else {
+					graph->adjLists[v1] = adj->next;
+				}
+				struct node* del = adj;
+				adj = adj->next;
+				free(del);
+				break;
+			}
+			else {
+				p = adj;
+				adj = adj->next;
+			}
+		}
+
+		adj = graph->adjLists[v2];
+		p = NULL;
+
+		while (adj) {
+			if (adj->vertex == v1) {
+				if (p) {
+					p->next = adj->next;
+				}
+				else {
+					graph->adjLists[v2] = adj->next;
+				}
+				struct node* del = adj;
+				adj = adj->next;
+				free(del);
+				break;
+			}
+			else {
+				p = adj;
+				adj = adj->next;
+			}
+		}
+
+		struct node* temp = graph->adjLists[v2];
+		while (temp) {
+			addEdge(graph, v1, temp->vertex);
+			temp = temp->next;
+		}
+
+		for (int i = 0; i < graph->numVertices; i++) {
+			adj = graph->adjLists[i];
+			p = NULL;
+
+			while (adj) {
+				if (adj->vertex == v2) {
+					if (p) {
+						p->next = adj->next;
+					}
+					else {
+						graph->adjLists[i] = adj->next;
+					}
+					struct node* del = adj;
+					adj = adj->next;
+					free(del);
+				}
+				else {
+					p = adj;
+					adj = adj->next;
+				}
+			}
+		}
+
+		graph->adjLists[v2] = NULL;
+
+		for (int i = v2 + 1; i < graph->numVertices; i++) {
+			graph->adjLists[i - 1] = graph->adjLists[i];
+		}
+		graph->adjLists[graph->numVertices - 1] = NULL;
+
+		graph->numVertices--;
+
+		for (int i = 0; i < graph->numVertices; i++) {
+			struct node* adj = graph->adjLists[i];
+			while (adj) {
+				if (adj->vertex > v2) {
+					adj->vertex--;
+				}
+				adj = adj->next;
+			}
 		}
 	}
-	for (int i = 0; i < size; i++) {
-		free(G[i]);
-	}
-	free(G);
-	G = NULL;
-
-	return Gtemp;
+	else { return; }
 }
 
+
+void splitV(struct Graph* graph, int v) {
+	if (v < 0 || v >= graph->numVertices) return;
+
+	int newV1 = graph->numVertices;
+	int newV2 = graph->numVertices + 1;
+
+	graph->numVertices += 1;
+	graph->adjLists = (struct node**)realloc(graph->adjLists, graph->numVertices * sizeof(struct node*));
+	graph->adjLists[newV1] = NULL;
+
+	struct node* temp = graph->adjLists[v];
+	while (temp) {
+		if (temp->vertex != v) {
+			addEdge(graph, newV1, temp->vertex);
+		}
+		temp = temp->next;
+	}
+}
 
 int main(void) {
-	int sizeG1 = 0, sizeG2 = 0, sizeG4 = 0, sizeG5 = 0;
+	int sizeG1 = 0, sizeG2 = 0, sizeG3 = 0;
 	int** G1 = NULL;
 	int** G2 = NULL;
 	int** G3 = NULL;
 
-	int** G4 = NULL;
-	int** G44 = NULL;
-
-	int** G5 = NULL;
-	int** G55 = NULL;
-
-	printf("-=-=-=-=-=-ZADANIE 1-=-=-=-=-=-=\n");
+	printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
 	printf("Vvedite kol-vo versh 1 grafa: ");
 	scanf("%d", &sizeG1);
-	printf("Vvedite kol-vo versh 2 grafa: ");
-	scanf("%d", &sizeG2);
-
 	G1 = createG(sizeG1);
 	printf("1 graf\n");
 	printG(G1, sizeG1);
+	struct Graph* graph1 = edges(G1, sizeG1);
+	printGraph(graph1);
 
+	int v1, v2;
+	printf("Vvedite vershiny dlya otoj v graph1: ");
+	scanf("%d %d", &v1, &v2);
+	unionV(graph1, v1 - 1, v2 - 1);
+	printf("Graf posle otoj:\n");
+	printGraph(graph1);
+
+	printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+	printf("Vvedite kol-vo versh 2 grafa: ");
+	scanf("%d", &sizeG2);
 	G2 = createG(sizeG2);
 	printf("2 graf\n");
 	printG(G2, sizeG2);
+	struct Graph* graph2 = edges(G2, sizeG2);
+	printGraph(graph2);
 
-	
+	int vert;
+	printf("Vvedite vershinu dlya raschepleniya v graph2: ");
+	scanf("%d", &vert);
+	splitV(graph2, vert - 1);
+	printf("Graf posle raschepleniya:\n");
+	printGraph(graph2);
 
-	printf("-=-=-=-=-=-ZADANIE 2-=-=-=-=-=-=\n");
-	int v1, v2;
-	printf("Versh dlya otoj grafa 1: ");
-	scanf("%d %d", &v1, &v2);
-	G3 = unionV(G1, sizeG1, v1 - 1, v2 - 1);
-	printf("3 graf otoj\n");
-	printG(G3, sizeG1 - 1);
+	printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+	printf("Vvedite kol-vo versh 3 grafa: ");
+	scanf("%d", &sizeG3);
+	G3 = createG(sizeG3);
+	printf("1 graf\n");
+	printG(G3, sizeG3);
+	struct Graph* graph3 = edges(G3, sizeG3);
+	printGraph(graph3);
 
 	int v3, v4;
-	printf("Vvedite kol-vo versh 4 grafa: ");
-	scanf("%d", &sizeG4);
-
-	G4 = createG(sizeG4);
-	printf("4 graf: \n");
-	printG(G4, sizeG4);
-
-	printf("Versh dlya udaleniya mejdu rebro: ");
+	printf("Vvedite vershiny dlya stjagivaniya v graph3: ");
 	scanf("%d %d", &v3, &v4);
-	G44 = contrE(G4, sizeG4, v3 - 1, v4 - 1);
-	printf("4 graf contrE^\n");
-	printG(G44, sizeG4-1);
-
-	int v5;
-	printf("Vvedite kol-vo versh 5 grafa: ");
-	scanf("%d", &sizeG5);
-
-	G5 = createG(sizeG5);
-	printf("5 graf: \n");
-	printG(G5, sizeG5);
-
-	printf("Versh dlya copirky: ");
-	scanf("%d", &v5);
-	G55 = splitV(G5, sizeG5, v5 - 1);
-	printf("5 graf split\n");
-	printG(G55, sizeG5+1);
-}
+	styag(graph3, v3 - 1, v4 - 1);
+	printf("Graf posle stjagivaniya:\n");
+	printGraph(graph3);
+}// добавить связь с расщипляемой вершиной
